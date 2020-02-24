d625c0cfb7972d4d40e47f1dca7b6e3d67cb2d2f
Peter Fight <alberto.piedrafits@gmail.com>
2020-02-25 00:20:18 +0100

    dsfasadfas

---
+++ TestGit/App.java
@@ -0,0 +1,13 @@
+package es.uned.masterjava.TestGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World testing git" );
+    }
+}
